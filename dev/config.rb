7c0b421102e4f474a0342a07c6052c948917c6bd
Mark Cooper <markchristophercooper@gmail.com>
2018-07-09 11:20:37 -0700

    Fix broken top container search

--- dev/config.rb
+++ dev/config.rb
@@ -17,7 +17,7 @@ AppConfig[:container_management_extent_calculator] = {
 }
 
 AppConfig[:digitization_work_order_ladybird]   = true
-AppConfig[:max_top_container_results]        = '2500'
+AppConfig[:max_top_container_results]          = 2500
 AppConfig[:preservica_data_deleter_match_url]  = "https://preservica.library.yale.edu"
 AppConfig[:preservica_data_deleter_match_user] = "preservicaprod"
 
