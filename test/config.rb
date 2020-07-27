3aaa8fb3834e9be434e029f562f06de3772eebce
SteelsenS <steelsen.smith@yale.edu>
2020-07-27 13:53:07 -0400

    also fix capitalization to comply with Lyrasis variable naming convention

--- test/config.rb
+++ test/config.rb
@@ -243,7 +243,7 @@ AppConfig[:record_inheritance] = {
   }
 }
 
-AppConfig[:google_maps_api_key] = ENV.fetch('google_maps_api_key', nil)
+AppConfig[:google_maps_api_key] = ENV.fetch('GOOGLE_MAPS_API_KEY', nil)
 AppConfig[:libcal_service_id] = '457'
 
 AppConfig[:omniauthCas] = {
