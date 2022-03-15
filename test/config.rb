57bcfc93b5c79db54f5ff49d5e611bda187faf14
Trip Kirkpatrick <trip.kirkpatrick@yale.edu>
2022-03-15 11:14:14 -0400

    Activated killswitch for digital copy requesting ☠️

--- test/config.rb
+++ test/config.rb
@@ -231,6 +231,8 @@ AppConfig[:aeon_fulfillment_record_types] = [
   "top_container"
 ]
 
+AppConfig[:aeon_disable_photoduplication] = true
+
 AppConfig[:aeon_client_max_results] = 5000
 AppConfig[:aeon_client_username] = 's_lib_t_aeon_hm'
 AppConfig[:aeon_client_password] = ENV.fetch('AEON_CLIENT_PASSWORD', nil)
